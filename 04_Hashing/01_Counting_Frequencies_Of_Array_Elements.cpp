62de1ee34e8f5fbb0d645d4e30ad5ef5e3792d34
Aashish Raj <iamtheaashish@gmail.com>
2026-06-24 13:21:03 +0530

    Remove incomplete frequency counting implementation from Counting Frequencies of Array Elements

--- 04_Hashing/01_Counting_Frequencies_Of_Array_Elements.cpp
+++ 04_Hashing/01_Counting_Frequencies_Of_Array_Elements.cpp
@@ -4,27 +4,6 @@ using namespace std;
 
 int main()
 {
-   int n;
-   cin >> n;
-   vector<int> arr(n);
-   for(int i = 0; i<n; i++){
-        cin >> arr[i];
-   }
-
-   // precompute
-   int hash[13] = {0};
-   for(int i=0; i<n; i++){
-        hash[arr[i]] += i;
-   }
-
-   int q;
-   cin >> q;
-   while(q--){
-        int number;
-        cin >> number;
-        // fetch
-        cout << hash[number] << endl;
-   }
-
+     // Fuck it, I was not able to understand. Will come back to it later.
     return 0;
 }
