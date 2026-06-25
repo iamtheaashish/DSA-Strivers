c1d83ee6fb7380bf88bc8e9118e2483fb4ded8de
Aashish Raj <iamtheaashish@gmail.com>
2026-06-25 13:06:45 +0530

    Implement selection sort algorithm and update main function for input/output

--- 05_Sorting-1/01_Selection_Sort.cpp
+++ 05_Sorting-1/01_Selection_Sort.cpp
@@ -1,7 +1,33 @@
 #include "../dsa.hpp"
 using namespace std;
 
+// ascending order
+void selection_sort(int arr[], int n){
+    for(int i = 0; i<=n-2;i++){
+        int mini = i;
+        for(int j = i; j<=n-1; j++){
+            if(arr[j] < arr[mini]){
+                mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
+}
+
 int main()
 {
+    const int n = 6;
+    int arr[n];
+
+    for(int i = 0; i<n; i++) cin >> arr[i];
+
+    selection_sort(arr, n);
+
+    for(int i = 0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
